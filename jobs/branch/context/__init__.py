068dd727500904c26678add0bcda76b3983f5f4a
Nikos Kallergis <nikolaos.kallergis@gmail.com>
2025-05-17 15:15:43 +0300

    Take four

--- jobs/branch/context/__init__.py
+++ jobs/branch/context/__init__.py
@@ -1,8 +1,11 @@
 """This module contains the render context for the basic design."""
+
+from django.core.exceptions import ObjectDoesNotExist
 from netaddr import IPNetwork
 
 from nautobot_design_builder.errors import DesignValidationError
 from nautobot_design_builder.context import Context, context_file
+from nautobot.dcim.models import Location
 from nautobot.ipam.models import Prefix
 
 @context_file("context.yaml")
@@ -13,15 +16,21 @@ class BaseDataContext(Context):
 class BranchDesignContext(Context):
     """Render context for branch design."""
 
+    def get_next_prefix(self):
+        """Get next available prefix."""
+        return "1.2.3.0/24"
+
     @property
     def branch_prefixes(self):
         """Calculate the branch prefixes."""
-        from rich import inspect
-        inspect(self)
         try:
-            supernet = Prefix.objects.get(location=self.site_name, role__name="Branch:Supernet")
-        except:
-            supernet = "1.2.3.0/24"
+            location = Location.objects.get(name=self.site_name)
+        except ObjectDoesNotExist:
+            supernet = self.get_next_prefix()
+        try:
+            supernet = Prefix.objects.get(location=location, role__name="Branch:Supernet")
+        except ObjectDoesNotExist:
+            supernet = self.get_next_prefix()
         return {
             "supernet": supernet,
         }
