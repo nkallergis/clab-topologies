a4c79e8a21097da584d228aebbcc41a06bf34e2c
Nikos Kallergis <nikolaos.kallergis@gmail.com>
2025-05-17 13:58:39 +0300

    Create base prefix

--- jobs/branch/context/__init__.py
+++ jobs/branch/context/__init__.py
@@ -1,10 +1,14 @@
 """This module contains the render context for the basic design."""
+from netaddr import IPNetwork
+
+from nautobot_design_builder.errors import DesignValidationError
 from nautobot_design_builder.context import Context, context_file
 
 
+@context_file("basedata.context.yaml")
 class BaseDataContext(Context):
     """Render context for base data."""
 
-@context_file("context.yaml")
+@context_file("branchdesign.context.yaml")
 class BranchDesignContext(Context):
     """Render context for branch design."""
