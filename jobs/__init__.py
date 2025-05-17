11c8030c0c39a62bdc364a92cbfaf11ff0258b41
Nikos Kallergis <nikolaos.kallergis@gmail.com>
2025-05-17 12:50:03 +0300

    duh duh

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,7 +1,8 @@
 """The __init__.py module is required for Nautobot to load the jobs via Git."""
 
-from .branch import BranchDesign
+from .branch import BaseData, BranchDesign
 
 __all__ = [
+    "BaseData",
     "BranchDesign",
 ]
