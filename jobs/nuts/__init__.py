7d9dca4f8b7ee4a2b6c437d85d96de6e7f4c5d5a
Nikos Kallergis <nikolaos.kallergis@gmail.com>
2025-05-18 13:54:05 +0300

    Try to disable NUTS logging

--- jobs/nuts/__init__.py
+++ jobs/nuts/__init__.py
@@ -20,11 +20,12 @@ class NutJob(Job):
     def run(self, **data):
         """Run NUTS tests."""
         self.logger.info("Running NUTS tests...")
+        
         pwd = Path(__file__).parent
-        self.logger.info(f"Current working directory: {pwd}")
+        log_level = self.logger.level
+        self.logger.setLevel("ERROR")
         result = pytest.main(["-q", "--disable-warnings", pwd / "tests"])
-        from pprint import pprint
-        pprint(result)
-        return 
+        self.logger.setLevel(log_level)
+        return result
 
 register_jobs(NutJob)
