91cd3ac70129073f14798f9d8c6f7f8706aa60b8
Nikos Kallergis <nikolaos.kallergis@gmail.com>
2025-05-18 13:17:46 +0300

    Add NutJob

---
+++ jobs/nuts/__init__.py
@@ -0,0 +1,26 @@
+"""Module to run NUTS tests from Nautobot."""
+
+import pytest
+
+from nautobot.apps.jobs import Job, register_jobs
+
+name = "NUTS"
+
+class NutJob(Job):
+    """A job to run NUTS tests."""
+
+    class Meta:
+        """Metadata for the NutJob."""
+
+        name = "NUTS Job"
+        description = "A job to test NUTS."
+        has_sensitive_variables = False
+
+    def run(self, **data):
+        """Run the NUTS tests."""
+        # This is where you would run your NUTS tests
+        # For now, we'll just print a message
+        self.logger.info("Running NUTS tests...")
+        return True
+
+register_jobs(NutJob)
